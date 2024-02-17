6fe4ad018976b1024716ed64c38ac531e56219ba
HanzDaoang <hanzdaoang@gmail.com>
2024-02-17 15:16:03 -0500

    fixed filters but buttons change the thing

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,32 @@ import './App.css';
 
 function App() {
   const [filters, setFilters] = useState({
-    filter1: false,
-    filter2: false,
-    filter3: false,
-    filter4: false,
-    filter5: false,
-    filter6: false,
+    showAll: true,
+    academics: false,
+    opportunities: false,
+    resources: false,
   });
 
   const handleFilterChange = (filter) => {
-    setFilters((prevFilters) => ({ ...prevFilters, [filter]: !prevFilters[filter] }));
+    setFilters((prevFilters) => ({
+      ...Object.fromEntries(Object.keys(prevFilters).map(key => [key, false])), // Reset all filters
+      [filter]: true,
+    }));
   };
 
   return (
     <div className="app">
       <aside className="sidebar">
         <div className="filters">
-          {Array.from({ length: 6 }, (_, index) => index + 1).map((filterNum) => (
-            <div key={filterNum} className="filter">
+          {Object.keys(filters).map((filter) => (
+            <div key={filter} className="filter">
               <label>
                 <input
                   type="checkbox"
-                  checked={filters[`filter${filterNum}`]}
-                  onChange={() => handleFilterChange(`filter${filterNum}`)}
+                  checked={filters[filter]}
+                  onChange={() => handleFilterChange(filter)}
                 />
-                Filter {filterNum}
+                {filter === 'showAll' ? 'Show All' : filter.charAt(0).toUpperCase() + filter.slice(1)}
               </label>
             </div>
           ))}
@@ -37,30 +38,55 @@ function App() {
       <div className="content">
         <h1>Mason Hub</h1>
         <div className="card">
-          <button onClick={() => window.location.href='https://blackboard.gmu.edu'}>
+          {filters.showAll && (
+            <>
+              <button onClick={() => window.location.href='https://blackboard.gmu.edu'}>
+              Blackboard
+              </button>
+              <button onClick={() => window.location.href='https://https://patriotweb.gmu.edu/'}>
+                PatriotWeb
+              </button>
+              <button onClick={() => window.location.href='https://https://mason360.gmu.edu/home_login'}>
+                Mason360
+              </button>
+              <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
+                Blackboard
+              </button>
+              <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
+                Blackboard
+              </button>
+              <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
+                Blackboard
+              </button>
+              <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
+                Blackboard
+              </button>
+              <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
+                Blackboard
+              </button>
+            </>
+          )}
+          {filters.academics && (
+            <>
+            <button onClick={() => window.location.href='https://blackboard.gmu.edu'}>
             Blackboard
-          </button>
-          <button onClick={() => window.location.href='https://patriotweb.gmu.edu'}>
-            PatriotWeb
-          </button>
-          <button onClick={() => window.location.href='https://mason360.gmu.edu'}>
-            Mason360
-          </button>
-          <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
-            Blackboard
-          </button>
-          <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
-            Blackboard
-          </button>
-          <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
-            Blackboard
-          </button>
-          <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
-            Blackboard
-          </button>
-          <button onClick={() => window.location.href='https://shibboleth.gmu.edu/idp/profile/SAML2/POST/SSO?execution=e1s1'}>
-            Blackboard
-          </button>
+            </button>
+          </>
+          )}
+          {filters.opportunities && (
+            <>
+            <button onClick={() => window.location.href='https://https://patriotweb.gmu.edu/'}>
+              PatriotWeb
+            </button>
+          </>
+          )}
+          {filters.resources && (
+            <>
+            <button onClick={() => window.location.href='https://https://mason360.gmu.edu/home_login'}>
+              Mason360
+            </button>
+          </>
+          )}
         </div>
       </div>
     </div>
