bec9dadfe4a23bc9bf3b4e195596120804f1a326
jealmonte <almontejoshua2@gmail.com>
2024-02-18 15:26:15 -0500

    Topbar

--- src/App.jsx
+++ src/App.jsx
@@ -32,71 +32,70 @@ function App() {
 			<div className='Header'>
 				<h1>Mason Hub</h1>
 			</div>
-      <div className="top-bar">
+	<div className="top-bar">
         <div className="dropdown">
           <button className="dropbtn" onClick={() => handleFilterChange('finance')}>Finance</button>
           <div className="dropdown-content">
-            <a href="https://studentaccounts.gmu.edu/">Account Inquiries</a>
-            <a href="#">Make a Payment</a>
-            <a href="#">Scholarships</a>
-            <a href="#">Financial Aid</a>
-            <a href="#">TN Payment</a>
+            <a href="https://studentaccounts.gmu.edu/" target="_blank">Account Inquiries</a>
+            <a href="https://secure.touchnet.com/C20788_tsa/web/login.jsp" target="_blank">Make a Payment</a>
+            <a href="https://gmu.academicworks.com/" target="_blank">Scholarships</a>
+            <a href="https://www.gmu.edu/financial-aid" target="_blank">Financial Aid</a>
+            <a href="https://secure.touchnet.com/C20788_tsa/web/login.jsp" target="_blank">TN Payment</a>
           </div>
         </div>
 
         <div className="dropdown">
           <button className="dropbtn" onClick={() => handleFilterChange('academics')}>Academics</button>
           <div className="dropdown-content">
-            <a href="#">Course Overview</a>
-            <a href="#">Grades</a>
-            <a href="#">Class Schedule</a>
-            <a href="#">View Unofficial Transcript</a>
-            <a href="#">View GPA</a>
-            <a href="#">Official Transcript Request</a>
-            <a href="https://advising.gmu.edu/current_students/gpa_calculator.html">GPA Calculator</a>
-            <a href="#">Apply for Graduation</a>
+            <a href="https://mymasonportal.gmu.edu/ultra/course" target="_blank">Course Overview</a>
+            <a href="https://mymasonportal.gmu.edu/ultra/grades" target="_blank">Grades</a>
+            <a href="https://patriotweb.gmu.edu/" target="_blank">View Unofficial Transcript</a>
+            <a href="https://patriotweb.gmu.edu/" target="_blank">View GPA</a>
+            <a href="https://www.parchment.com/u/registration/34693/institution" target="_blank">Official Transcript Request</a>
+            <a href="https://advising.gmu.edu/current_students/gpa_calculator.html" target="_blank">GPA Calculator</a>
+            <a href="https://registrar.gmu.edu/students/graduation/process/" target="_blank">Apply for Graduation</a>
           </div>
         </div>
 
         <div className="dropdown">
           <button className="dropbtn" onClick={() => handleFilterChange('resources')}>Resources</button>
           <div className="dropdown-content">
-            <a href="#">Writing Center</a>
-            <a href="#">Campus Map</a>
-            <a href="#">Virtual Tutoring</a>
-            <a href="#">Learning Services</a>
-            <a href="#">Center for Psychological Services</a>
-            <a href="#">Student Health Services</a>
+            <a href="https://writingcenter.gmu.edu/" target="_blank">Writing Center</a>
+            <a href="https://info.gmu.edu/campus-maps-and-directions/" target="_blank">Campus Map</a>
+            <a href="https://ds.gmu.edu/tutoring-services/" target="_blank">Virtual Tutoring</a>
+            <a href="https://learningservices.gmu.edu/" target="_blank">Learning Services</a>
+            <a href="https://ccmh.gmu.edu/" target="_blank">Center for Psychological Services</a>
+            <a href="https://shs.gmu.edu/" target="_blank">Student Health Services</a>
           </div>
         </div>
 
         <div className="dropdown">
           <button className="dropbtn" onClick={() => handleFilterChange('extracurriculars')}>Extracurriculars</button>
           <div className="dropdown-content">
-            <a href="#">Student Involvement Calendar</a>
-            <a href="#">GMU Sport Events</a>
-            <a href="#">Fraternity & Sorority Life</a>
-            <a href="#">Clubs and Organizations</a>
+            <a href="https://si.gmu.edu/calendar/" target="_blank">Student Involvement Calendar</a>
+            <a href="https://gomason.com/calendar" target="_blank">GMU Sport Events</a>
+            <a href="https://si.gmu.edu/fsl/" target="_blank">Fraternity & Sorority Life</a>
+            <a href="https://www.gmu.edu/student-life/clubs-and-organizations" target="_blank">Clubs and Organizations</a>
           </div>
         </div>
 
         <div className="dropdown">
           <button className="dropbtn" onClick={() => handleFilterChange('enrollment')}>Enrollment</button>
           <div className="dropdown-content">
-          <a href="#">Add Classes</a>
-            <a href="#">Drop Classes</a>
-            <a href="#">Class Schedule</a>
-            <a href="#">Enrollment Dates</a>
+          <a href="https://masononline.gmu.edu/student-resources/register/" target="_blank">Add Classes</a>
+            <a href="https://publichealth.gmu.edu/students/academic-policies-and-procedures/course-withdrawal" target="_blank">Drop Classes</a>
+            <a href="https://mymasonportal.gmu.edu/ultra/calendar" target="_blank">Class Schedule </a>
+            <a href="https://registrar.gmu.edu/calendars/spring_2024/" target="_blank">Enrollment Dates</a>
           </div>
         </div>
 
         <div className="dropdown">
           <button className="dropbtn" onClick={() => handleFilterChange('studentInfo')}>Student Info</button>
           <div className="dropdown-content">
-            <a href="#">Personal Information</a>
-            <a href="#">Student Records</a>
-            <a href="#">Buy Parking Permit</a>
-            <a href="#">Transportation Programs</a>
+            <a href="https://patriotweb.gmu.edu/" target="_blank">Personal Information</a>
+            <a href="https://patriotweb.gmu.edu/" target="_blank">Student Records</a>
+            <a href="https://gmu.t2hosted.com/Account/Portal" target="_blank">Parking Portal</a>
+            <a href="https://transportation.gmu.edu/transportation-services/" target="_blank">Transportation Programs</a>
           </div>
         </div>
       </div>
